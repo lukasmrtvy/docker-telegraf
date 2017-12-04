d0ad309f6f3c49b6b48c95aef27ec60cc1057a58
Lukas M <lukas.mrtvy@gmail.com>
2017-12-04 10:20:06 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,14 @@
 #!/bin/sh
 set -e
 
+if [ -z "${NETATMO_CLIENT_ID+x}" ] || [ -z "${NETATMO_CLIENT_SECRET+x}" ] || [ -z "${NETATMO_USERNAME+x}" ] || [ -z "${NETATMO_PASSWORD+x}" ] || [ -z "${NETATMO_DEVICE_ID+x}" ] ;  then
+    
+   echo "You must set env variables for netatmo plugin !"
+   exit 1
+
+fi
+
+
 if [ "${1:0:1}" = '-' ]; then
     set -- telegraf "$@"
 fi
